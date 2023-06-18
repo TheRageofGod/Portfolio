5ac8db2ea829eba17a610451e9c16413edc19d0a
TheRageofGod <spacedwarf1@gmail.com>
2023-06-19 01:49:13 +1000

    Basic Movement

--- Portfolio/Assets/Projects/Player Controller/Scripts/PlayerController.cs
+++ Portfolio/Assets/Projects/Player Controller/Scripts/PlayerController.cs
@@ -4,42 +4,70 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Transform playerCamera = null;//Reffrences Camera
+    [SerializeField] float mouseSensitivity = 3.5f;//Controls Mouse Sensitivity
+    [SerializeField][Range(0.0f, 0.5f)] float mouseSmoothTime = 0.3f;//controls how smooth the Camera movement is
 
-    private CharacterController controller; // refrences the Character Controller component on the Player Gameobject
-    private Vector3 playerVelocity;
-    public bool groundedPlayer;
-    public float playerSpeed = 2.0f; //Controls Player Speed
-    public float jumpHeight = 1.0f;//Controls how high the player can jump
-    public float gravityValue = -9.81f;// controls how "Heavy" the Gravity is
+    [SerializeField] float walkSpeed = 6.0f;//controls how fast you walk
+    [SerializeField][Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;//controls how smooth the movement is
+    [SerializeField] float gravity = -13.0f;//Gravity Value
+
+    [SerializeField] bool lockCursor = true;//bool to lock Cursor
+
+    CharacterController characterController = null;//CharacterController Refference
+    float cameraPitch = 0.0f;//Controls Camera Pitch
+    float velocityY = 0.0f;//Gravity Velocity
+
+    Vector2 currentDir = Vector2.zero;
+    Vector2 currentDirVelocity = Vector2.zero;
+
+    Vector2 currentMouseDelta = Vector2.zero;
+    Vector2 currentMouseDeltaVelocity = Vector2.zero;
 
     private void Start()
     {
-        controller = controller = GetComponent<CharacterController>();//Links the Component into the script
+        characterController = GetComponent<CharacterController>();//Plugs in the Controller on the object
+        if (lockCursor == true)
+        {
+            Cursor.lockState = CursorLockMode.Locked;//locks the cursor
+            Cursor.visible = false;//sets the cursor Invisible
+        }
+    }
+    private void Update()
+    {
+        UpdateCameraLook();//Calls the Camera look function
+        UpdateMovement();//Calls teh Movement Function
     }
 
-    void Update()
+    void UpdateCameraLook()
     {
-        groundedPlayer = controller.isGrounded;//Links the Grounded check on the controller into the script
-        if (groundedPlayer && playerVelocity.y < 0)//Checks if the Object is Grounded
+        Vector2 tragetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));//Gets the Mouse Inputs from Unitys systems
+
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, tragetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);//Smooth Camera
+
+        cameraPitch -= currentMouseDelta.y * mouseSensitivity;//controls the Vertical Mouse Pitch
+        cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);//sets the Clamp Angle
+
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;//Clamps the camera so you cant spin it
+
+        transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);//Controls the Horizontal Mouse look
+    } //Controls Camera Look
+
+    void UpdateMovement()//Controls Character Movement
+    {
+        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));//Stores the input values
+        targetDir.Normalize();//normalizes the Vectoprs so you dont go faster on diagonals
+
+        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);//smooths movement
+
+        if (characterController.isGrounded == true)
         {
-            playerVelocity.y = 0f;//Sets velocity to 0 enabling smooth movement
-        }
+            velocityY = 0.0f;
+            velocityY += gravity * Time.deltaTime;
+        }//Defines Gravity
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));//Creates a new Velocity based on the Axis inputs
-        controller.Move(move * Time.deltaTime * playerSpeed);//Links controls with "real time" and the required speed
+        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * walkSpeed + Vector3.up * velocityY;//creates velocity for both movement and gravity
 
-        if (move != Vector3.zero)//enables Movement based on the inputs
-        {
-            gameObject.transform.forward = move;//Moves the object
-        }
-
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)//checks if the jump button has been pressed and if the object is on the ground
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);//Jump math
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;//inforces the Y Velocity
-        controller.Move(playerVelocity * Time.deltaTime);//inforces the other axis velocity
+        characterController.Move(velocity * Time.deltaTime);//Applies the Velocity and makes it independent of framerate
     }
 }
