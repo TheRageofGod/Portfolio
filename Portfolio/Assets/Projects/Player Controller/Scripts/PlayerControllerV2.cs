edeea08fe97ddd255be1e70da2e350509f84a0a0
TheRageofGod <spacedwarf1@gmail.com>
2023-06-22 12:16:00 +1000

    Started Reprograming Movement

--- Portfolio/Assets/Projects/Player Controller/Scripts/PlayerControllerV2.cs
+++ Portfolio/Assets/Projects/Player Controller/Scripts/PlayerControllerV2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleMove : MonoBehaviour
+public class PlayerControllerV2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
